fbd7931a42505f86a6b497a16d03147d2f4d2f08
Brett Snaidero <brettsnaidero@hotmail.com>
2016-08-31 19:08:33 +1000

    Polish

--- scripts/bot.js
+++ scripts/bot.js
@@ -4,4 +4,12 @@ module.exports = function(robot) {
     return res.send("Bye!");
   });
 
+  robot.respond(/is it a (weekend|holiday)\s?\?/i, function(msg){
+      let today = new Date();
+
+      msg.reply(
+        today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO"
+      );
+  });
+
 }
