d0d0ad599a8bb2f46181e689ab0851183d2d78d9
Brett Snaidero <brettsnaidero@hotmail.com>
2016-09-04 14:04:19 +1000

    Switch step

--- scripts/bot.js
+++ scripts/bot.js
@@ -73,31 +73,15 @@ module.exports = function(robot) {
 
       chosenDoor = memoryAnswer;
 
-      // // Convert to number
-      switch (memoryAnswer) {
-        case 'One':
-          memoryAnswer = 1;
-          break;
-        case 'Two':
-          memoryAnswer = 2;
-          break;
-        case 'Three':
-          memoryAnswer = 3;
-          break;
-        default:
-          break;
-      }
-      memoryAnswer = memoryAnswer.parseInt();
+      numberAnswer = memoryAnswer.parseInt();
 
-      return msg.reply( memoryAnswer );
-      //
-      // if (memoryAnswer === 1 || memoryAnswer === 2 || memoryAnswer === 3) {
-      //   firstTurn(memoryAnswer - 1);
-      //   currentTurn++;
-      //   return msg.reply( "Excellent choice! The host then proceeds to open door number " + memoryAnswer + ". There's a goat behind the door! So the car is either behind your chosen door, or the other remaining closed door. She offers you a choice: you can choose to stick with your original choice, or swap your choice to the remaining unclosed door. Would you like to switch? (Format: 'Switch Yes/No')"  );
-      // } else {
-      //   return msg.reply( "Sorry, didn't understand that." );
-      // }
+      if (numberAnswer === 1 || numberAnswer === 2 || numberAnswer === 3) {
+        firstTurn(numberAnswer - 1);
+        currentTurn++;
+        return msg.reply( "Excellent choice! The host then proceeds to open door number " + memoryAnswer + ". There's a goat behind the door! So the car is either behind your chosen door, or the other remaining closed door. She offers you a choice: you can choose to stick with your original choice, or swap your choice to the remaining unclosed door. Would you like to switch? (Format: 'Switch Yes/No')"  );
+      } else {
+        return msg.reply( "Sorry, didn't understand that." );
+      }
 
     });
 
